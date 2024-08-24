48cd07ef6e39bbc9e6ec98366d4f796fba888339
wang <1509326266@qq.com>
2024-08-24 20:35:11 +0800

    chore(projects): add vscode configuration & add vite preload

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import process from 'node:process';
 import { defineConfig, loadEnv } from 'vite';
 import { setupVitePlugins } from './build/plugins/index';
 import { createViteProxy, getBuildTime } from './build/config';
+import { include } from './build/optimize';
+
 // https://vitejs.dev/config/
 export default defineConfig(configEnv => {
   const viteEnv = loadEnv(configEnv.mode, process.cwd()) as unknown as Env.ImportMeta;
@@ -15,6 +17,7 @@ export default defineConfig(configEnv => {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
+    optimizeDeps: { include },
     css: {
       preprocessorOptions: {
         scss: {
@@ -30,6 +33,9 @@ export default defineConfig(configEnv => {
       host: '0.0.0.0',
       port: 9527,
       open: true,
+      warmup: {
+        clientFiles: ['./index.html', './src/{pages,components}/*']
+      },
       proxy: createViteProxy(viteEnv, configEnv.command === 'serve'),
       fs: {
         cachedChecks: false
