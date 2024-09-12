fbe7ddb3b0c9541adfcea310e9c9cc87cea69963
DESKTOP-31IBRMI\Administrator <1509326266@qq.com>
2024-09-12 15:51:41 +0800

    fix(projects): fix refresh token when meet multi requests

--- src/service/request/shared.ts
+++ src/service/request/shared.ts
@@ -1,33 +1,47 @@
-import type { AxiosRequestConfig } from 'axios';
 import { resetStore } from '@/store/slice/auth';
 import { store } from '@/store';
 import { localStg } from '@/utils/storage';
 import { fetchRefreshToken } from '../api';
 import type { RequestInstanceState } from './type';
 
+export function getAuthorization() {
+  const token = localStg.get('token');
+  const Authorization = token ? `Bearer ${token}` : null;
+
+  return Authorization;
+}
+
 /**
  * refresh token
  *
  * @param axiosConfig - request config when the token is expired
  */
-export async function handleRefreshToken(axiosConfig: AxiosRequestConfig) {
+export async function handleRefreshToken() {
   const refreshToken = localStg.get('refreshToken') || '';
   const { error, data } = await fetchRefreshToken(refreshToken);
   if (!error) {
     localStg.set('token', data.token);
     localStg.set('refreshToken', data.refreshToken);
-
-    const config = { ...axiosConfig };
-    if (config.headers) {
-      config.headers.Authorization = data.token;
-    }
-
-    return config;
+    return true;
   }
 
   store.dispatch(resetStore());
 
-  return null;
+  return false;
+}
+
+export async function handleExpiredRequest(state: RequestInstanceState) {
+  if (!state.refreshTokenFn) {
+    state.refreshTokenFn = handleRefreshToken();
+  }
+
+  const success = await state.refreshTokenFn;
+
+  setTimeout(() => {
+    state.refreshTokenFn = null;
+  }, 1000);
+
+  return success;
 }
 
 export function showErrorMsg(state: RequestInstanceState, message: string) {
