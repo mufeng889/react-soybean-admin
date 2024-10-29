a4b514cf06f77a452ac5272b000b17114130afe5
COBORGA-8FFVDVM\cob <1509326266@qq.com>
2024-10-29 15:14:25 +0800

    fix(projects): login after don't go login page .close #14

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -33,7 +33,12 @@ export const init: Init = async currentFullPath => {
 
     return location;
   }
+
   await store.dispatch(initAuthRoute());
+
+  if (currentFullPath.includes('login')) {
+    return { name: 'root', replace: true };
+  }
 
   return null;
 };
